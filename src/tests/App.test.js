c8053321a0fce982df1b0230f0ea273f2e015a41
Cristiano <weydsoncristiano@hotmail.com>
2022-10-21 19:42:07 -0300

    andamento dos testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Teste o componente <App.js />', () => {
+  test('verificar os data testId', () => {
+    render(<App />);
+    const inputTestId = screen.getByTestId('name-filter');
+    expect(inputTestId).toBeInTheDocument();
+
+    const inputValueTestId = screen.getByTestId('value-filter');
+    expect(inputValueTestId).toBeInTheDocument();
+
+    const botaoTestId = screen.getByTestId('button-filter');
+    expect(botaoTestId).toBeInTheDocument();
+
+    const botaoRemoverTestId = screen.getByTestId('button-remove-filters');
+    expect(botaoRemoverTestId).toBeInTheDocument();
+
+    const columTestId = screen.getByTestId('column-filter');
+    expect(columTestId).toBeInTheDocument();
+  })
+})
